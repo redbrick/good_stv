5318beeb0fc1b9c04be43bbebe93ccd2160d6da1
cbutler <cbutler@demonware.net>
2019-01-16 11:06:54 +0000

    remove templating and frontend

--- app/index.js
+++
@@ -1,9 +0,0 @@
-import { Application } from 'stimulus';
-import { definitionsFromContext } from 'stimulus/webpack-helpers';
-import Turbolinks from 'turbolinks';
-import './styles/main.scss';
-
-Turbolinks.start();
-const application = Application.start();
-const context = require.context('./controllers', true, /\.js$/);
-application.load(definitionsFromContext(context));

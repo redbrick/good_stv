9580fcd6c97458e6ea02824e19c37be6440470af
Terry Bolt <tbolt+github@google.com>
2019-01-24 15:18:04 +0000

    Implement creation and retrieval of polls.
    Right now we're just using a HashMap with a RwLock around it for our
    storage. Obviously this will be changed to use a DB, but I think getting
    all the endpoints working is higher priority.

--- src/bin/server/routes.rs
+++ src/bin/server/routes.rs
@@ -1,5 +1,5 @@
 /* good_stv - a good single transferable vote utility.
- * Copyright (C) 2017 Terry Bolt
+ * Copyright (C) 2019 good_stv authors
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -14,10 +14,69 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <https://www.gnu.org/licenses/>. */
 
-use std::string::String;
+use std::iter;
 
-use rocket::Request;
-use rocket::{catch};
+use log::info;
+use rand::Rng;
+use rocket::response::content;
+use rocket::*;
+use rocket_contrib::json::Json;
+use serde_json::json;
+
+use crate::poll::{Poll, PollCreationRequest};
+use crate::PollDb;
+
+const ADMIN_KEY_LENGTH: usize = 8;
+
+#[post("/polls", data = "<poll_req>")]
+pub fn create_poll(
+    poll_req: Json<PollCreationRequest>,
+    poll_db: State<PollDb>,
+) -> response::status::Created<content::Json<String>> {
+    let poll = Poll::new(poll_req.name.clone(), poll_req.candidates.clone());
+    info!("Created poll: {:#?}", poll);
+
+    let poll_id = poll.id.clone();
+    poll_db.polls.write().unwrap().insert(poll_id.clone(), poll);
+
+    let admin_key: String = iter::repeat(())
+        .map(|()| rand::thread_rng().sample(rand::distributions::Alphanumeric))
+        .take(ADMIN_KEY_LENGTH)
+        .collect();
+    let res = json!({
+        "id": poll_id.clone(),
+        "admin_key": admin_key,
+    });
+    response::status::Created(
+        uri!(get_poll: poll_id).to_string(),
+        Some(content::Json(res.to_string())),
+    )
+}
+
+#[get("/polls/<id>")]
+pub fn get_poll(id: String, poll_db: State<PollDb>) -> Option<content::Json<String>> {
+    poll_db
+        .polls
+        .read()
+        .unwrap()
+        .get(&id)
+        .map(|poll| content::Json(serde_json::to_string(&poll).unwrap()))
+}
+
+#[post("/polls/<id>")]
+pub fn vote(id: String) {
+    unimplemented!()
+}
+
+#[post("/polls/<id>/results")]
+pub fn close_poll(id: String) {
+    unimplemented!()
+}
+
+#[get("/polls/<id>/results")]
+pub fn get_results(id: String) {
+    unimplemented!()
+}
 
 #[catch(500)]
 pub fn internal_error() -> &'static str {
